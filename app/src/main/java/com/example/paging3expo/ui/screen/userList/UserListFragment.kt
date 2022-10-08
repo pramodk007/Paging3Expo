1fe05a4e878da8e0875ead4ebb70a13383785141
Pramod Kumar <pramodk8001@gmail.com>
2022-10-08 06:39:14 +0530

    some upgrade.

--- app/src/main/java/com/example/paging3expo/ui/screen/userList/UserListFragment.kt
+++ app/src/main/java/com/example/paging3expo/ui/screen/userList/UserListFragment.kt
@@ -1,19 +1,26 @@
 package com.example.paging3expo.ui.screen.userList
 
 import android.os.Bundle
+import android.transition.Slide
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.paging3expo.databinding.FragmentUserListBinding
 import com.example.paging3expo.ui.screen.userList.adapter.UserListAdapter
 import com.example.paging3expo.ui.screen.userList.adapter.UserLoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -35,6 +42,13 @@ class UserListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initView()
         subscribeObserver()
+        binding.scrollUp.setOnClickListener {
+            lifecycleScope.launch {
+                binding.recyclerView.scrollToPosition(0)
+                delay(100)
+                binding.scrollUp.toggle(false)
+            }
+        }
     }
 
     private fun initView() {
@@ -45,7 +59,22 @@ class UserListFragment : Fragment() {
             adapter = userListAdapter.withLoadStateFooter(
                 footer = UserLoadStateAdapter{retry()}
             )
+            addOnScrollListener(object: RecyclerView.OnScrollListener(){
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val scrolledPostion =
+                        (recyclerView.layoutManager as? LinearLayoutManager)?.findFirstVisibleItemPosition()
 
+                    if(scrolledPostion != null){
+                        if (scrolledPostion >= 1){
+                            binding.scrollUp.toggle(true)
+                        }else{
+                            binding.scrollUp.toggle(false)
+                        }
+                    }
+                }
+            }
+            )
         }
     }
 
@@ -63,4 +92,12 @@ class UserListFragment : Fragment() {
         userListAdapter.retry()
     }
 
+}
+
+fun View.toggle(show: Boolean) {
+    val transition: Transition = Slide(Gravity.BOTTOM)
+    transition.duration = 200
+    transition.addTarget(this)
+    TransitionManager.beginDelayedTransition(this.parent as ViewGroup?, transition)
+    this.isVisible = show
 }
